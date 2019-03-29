c72b0fb8e08bf280d59e175a90fe6e421ddf8d1a
anisca22 <anisca22@yahoo.com>
2019-03-29 21:18:24 +0200

    Ionic Maps test (not working)

---
+++ mapsTest/src/app/home/home.page.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  map: GoogleMap;
+  constructor() { }
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+
+    // This code is necessary for browser
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': '(AIzaSyB0FtcG2mMqlA30UDrauw8X4dnMz_cRoR4)',
+      'API_KEY_FOR_BROWSER_DEBUG': '(AIzaSyB0FtcG2mMqlA30UDrauw8X4dnMz_cRoR4)'
+    });
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
+}
